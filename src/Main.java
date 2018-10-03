70f460f746d5e4f75a6eb82e4c81f5b1d8679b12
eoghanmcdermott <eoghan.mc-dermott.1@ucdconnect.ie>
2018-10-03 16:25:35 +0000

    Initial setup

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String args[]){
+
+       MixingBuffer mix = new MixingBuffer();
+
+       System.out.println(mix.toString());
+    }
+}
---
+++ src/MixingBuffer.java
@@ -0,0 +1,8 @@
+import javax.sound.sampled.*;
+
+public class MixingBuffer {
+
+    public MixingBuffer(){}//default constructor for now - will make more use later
+
+
+}
