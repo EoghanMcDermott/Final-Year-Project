2b0267165778396c5f52a36f2ce9ea01c0e5939f
eoghanmcdermott <eoghan.mc-dermott.1@ucdconnect.ie>
2018-10-03 20:44:20 +0000

    Added passing of files to buffer

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.io.File;
+
 public class Main {
 
     public static void main(String args[]){
 
-       MixingBuffer mix = new MixingBuffer();
+       File v1 = new File("voice_1.wav");
+       File v2 = new File("voice_2.wav");
+       File v3 = new File("voice_3.wav");
+       //audio samples
 
-       System.out.println(mix.toString());
+        MixingBuffer mix = new MixingBuffer(v1,v2,v3);//instantiate mixing buffer with audio files
+
+       System.out.println(mix.samplesToString());
     }
 }
--- src/MixingBuffer.java
+++ src/MixingBuffer.java
@@ -1,8 +1,22 @@
 import javax.sound.sampled.*;
+import java.io.File;
+import java.util.ArrayList;
 
-public class MixingBuffer {
+public class MixingBuffer {//placeholder class name for now
 
-    public MixingBuffer(){}//default constructor for now - will make more use later
+    private ArrayList<File> samples = new ArrayList<>();//instantiate list so can add to it
 
+    public MixingBuffer(File ... audioIn){//variable arguments to add audio files
 
+        for(File f: audioIn)
+            samples.add(f);
+    }
+
+    public void add(File f){//option to add audio files later
+        samples.add(f);
+    }
+
+    public String samplesToString(){
+        return samples.toString();
+    }
 }
