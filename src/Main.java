1d81d2ce79ad8eb49d472d00895795ae82dfd68e
Eoghan <eoghan.mc-dermott.1@ucdconnect.ie>
2018-11-15 00:03:08 +0000

    Reduced the gain in the softer samples

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ public class Main {
 
        MixingBuffer mix = new MixingBuffer();//instantiate mixing buffer with(out) audio files
 
-       mix.generateNormal();
-       mix.play("normal.wav");
+//       mix.generateNormal();
+//       mix.play("normal.wav");
 
        //audio samples are okay but aren't mixed properly together
        mix.generateSoft();
        mix.play("soft.wav");
 
-       mix.generateShout();
-       mix.play("shout.wav");
+//       mix.generateShout();
+//       mix.play("shout.wav");
 
 
     }
